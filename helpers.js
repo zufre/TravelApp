21ff77698259ac400e191e592af50723874e937a
Cédric David <cedric.david@btopenworld.com>
2018-12-23 18:49:33 +1000

    changed temp to c

--- helpers.js
+++ helpers.js
@@ -5,11 +5,11 @@ const createVenueHTML = (name, location, iconSource) => {
   <p>${location.address}</p>
   <p>${location.city}</p>
   <p>${location.country}</p>`;
-}
+};
 
-const createWeatherHTML = (currentDay) => {
-  return `<h2> High: ${currentDay.day.maxtemp_f}</h2>
-    <h2> Low: ${currentDay.day.mintemp_f}</h2>
+const createWeatherHTML = currentDay => {
+  return `<h2> High: ${currentDay.day.maxtemp_c}°C</h2>
+    <h2> Low: ${currentDay.day.mintemp_c}°C</h2>
     <img src="https://${currentDay.day.condition.icon}" class="weathericon" />
-    <h2>${weekDays[(new Date(currentDay.date)).getDay()]}</h2>`;
-}
+    <h2>${weekDays[new Date(currentDay.date).getDay()]}</h2>`;
+};
